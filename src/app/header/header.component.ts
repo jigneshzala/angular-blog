9b7c7ceddab4eec4684ddb1fa6df2bcbeb7ca924
jignesh zala <jigneshkumar1494@gmail.com>
2021-01-11 20:06:52 +0530

    minor fix

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../categories/shared/category.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService : CategoryService) { }
+  categories:any = [];
+  ngOnInit() {
+    this.getAllCategory();
+  }
 
-  ngOnInit(): void {
+  private getAllCategory() {
+    this.categoryService.getAllCateogry().subscribe((data) => {
+      this.categories = data;
+    });
   }
   updatemenu() {
   /*   if (document.getElementById('responsive-menu').checked == true) {
