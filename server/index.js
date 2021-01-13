7ad84a079683a7208c8fcf91e5e210bb459f1b92
jignesh zala <jigneshkumar1494@gmail.com>
2021-01-13 14:53:44 +0530

    added path module

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const config = require('./config');
+const path = require('path');
 const cors = require("cors");
 
 const dotenv = require('dotenv');
