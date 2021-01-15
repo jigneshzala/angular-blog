eb62143c9a44d5c84c874d63d7e2963a29d1df5e
jignesh zala <jigneshkumar1494@gmail.com>
2021-01-15 15:55:54 +0530

    seprated server part

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  DB_URI: 'mongodb+srv://jigar:nodeblog@cluster0.ztekr.mongodb.net/node-blog?retryWrites=true&w=majority',
-  JWT_SECRET: 'most-secure-and-long-secret',
-  CLOUDINARY_NAME: 'dvo6eiftd',
-  CLOUDINARY_KEY: '584656768329226',
-  CLOUDINARY_SECRET: 'qPcspwzbd8BQdB2DFm1c8Iv2YHI'
-
-}
--- server/routes/mediaRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const mediaController = require('./../controllers/mediaController');
-
-router.get('/', mediaController.getAllImages);
-router.delete('/:imageId', mediaController.deleteById);
-module.exports = router;
