465cdf400b1dc625082cf860887aafb0113eb625
jignesh zala <jigneshkumar1494@gmail.com>
2021-01-16 10:09:06 +0530

    build

--- src/app/posts/shared/post.service.ts
+++ src/app/posts/shared/post.service.ts
@@ -5,8 +5,8 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   // API_URL = "http://127.0.0.1:3000/";
-  // API_URL = "/api/v1/";
-  API_URL = "https://nodeblog-api.herokuapp.com/api/v1/";
+   API_URL = "/api/v1/";
+  //API_URL = "https://nodeblog-api.herokuapp.com/api/v1/";
 
   getPosts() {
     return this.http.get(`${this.API_URL}posts`);
