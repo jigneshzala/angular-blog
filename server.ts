320fe87c726dee15fcde6932185cf4dca897cd51
jignesh zala <jigneshkumar1494@gmail.com>
2021-01-30 13:03:23 +0530

    added dyanamic sitemap

--- server.ts
+++ server.ts
@@ -1,38 +1,64 @@
-import 'zone.js/dist/zone-node';
+import "zone.js/dist/zone-node";
 
-import { ngExpressEngine } from '@nguniversal/express-engine';
-import * as express from 'express';
-import { join } from 'path';
+import { ngExpressEngine } from "@nguniversal/express-engine";
+import * as express from "express";
+import { join } from "path";
 
-import { AppServerModule } from './src/main.server';
-import { APP_BASE_HREF } from '@angular/common';
-import { existsSync } from 'fs';
+import { AppServerModule } from "./src/main.server";
+import { APP_BASE_HREF } from "@angular/common";
+import { existsSync } from "fs";
+const request = require("request");
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
   const distFolder = join(process.cwd(), "dist", "browser");
+  const sitemapUrl = "https://nodeblog-api.herokuapp.com/sitemap.xml";
   // const distFolder = join(process.cwd(), 'dist/nodeblog/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const indexHtml = existsSync(join(distFolder, "index.original.html"))
+    ? "index.original.html"
+    : "index";
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
-    bootstrap: AppServerModule,
-  }));
+  server.engine(
+    "html",
+    ngExpressEngine({
+      bootstrap: AppServerModule,
+    })
+  );
 
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
+  server.set("view engine", "html");
+  server.set("views", distFolder);
+
+  // ---- SERVE SITEMAPS.XML FROM A DEDICATED API ---- //
+  server.all("*.xml", function (req, res) {
+    // we need to redirect the sitemap request directly to the backend
+    var options = {
+      url: sitemapUrl,
+      headers: {
+        Accept: "application/xml",
+      },
+    };
+
+    request(options).pipe(res);
+  });
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get(
+    "*.*",
+    express.static(distFolder, {
+      maxAge: "1y",
+    })
+  );
 
   // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
+  server.get("*", (req, res) => {
+    res.render(indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    });
   });
 
   return server;
@@ -53,9 +79,9 @@ function run() {
 // The below code is to ensure that the server is run only when not requiring the bundle.
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+const moduleFilename = (mainModule && mainModule.filename) || "";
+if (moduleFilename === __filename || moduleFilename.includes("iisnode")) {
   run();
 }
 
-export * from './src/main.server';
+export * from "./src/main.server";
