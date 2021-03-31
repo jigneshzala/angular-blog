1bdc3034b74c6b8c45f937f0a9458d61edcdf217
jignesh zala <jigneshkumar1494@gmail.com>
2021-03-31 20:06:48 +0530

    updated verfication

--- src/app/verification/verification.component.ts
+++ src/app/verification/verification.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { UserService } from '../admin/manage/users/shared/user.service';
+import { Component, Injector, OnInit, PLATFORM_ID } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { UserService } from "../admin/manage/users/shared/user.service";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
-  selector: 'app-verification',
-  templateUrl: './verification.component.html',
-  styleUrls: ['./verification.component.scss']
+  selector: "app-verification",
+  templateUrl: "./verification.component.html",
+  styleUrls: ["./verification.component.scss"],
 })
 export class VerificationComponent implements OnInit {
-
-  constructor(private userService : UserService,private route :ActivatedRoute) { }
-  responseText:any = '';
-  token:any = '';
+  platformId: any;
+  constructor(
+    injector: Injector,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {
+    this.platformId = injector.get(PLATFORM_ID);
+  }
+  responseText: any = "";
+  token: any = "";
 
   ngOnInit(): void {
-    
-    this.token = this.route.snapshot.params.token;
-    this.userService.verifySubscriber(this.route.snapshot.params.token).subscribe((response) => {
-        
-      this.responseText = "Verification success!";
-    },(error)=>{
-      this.responseText = error.error.error;
-      
-    })
-
+    if (isPlatformBrowser(this.platformId)) {
+      this.token = this.route.snapshot.params.token;
+      this.userService
+        .verifySubscriber(this.route.snapshot.params.token)
+        .subscribe(
+          (response) => {
+            this.responseText = "Verification success!";
+          },
+          (error) => {
+            this.responseText = error.error.error;
+          }
+        );
+    }
   }
-
 }
